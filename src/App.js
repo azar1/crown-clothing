dcb772c66bfdf9edeae00adc3143bd3e07aa90fb
azar1 <azarpathan05@gmmail.com>
2019-09-28 17:39:03 +0530

    added navigation to homepage components with router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
-
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => {
+  return <h1>Htas page</h1>;
+};
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop/hats" component={HatsPage} />
+      </Switch>
+      {/* <HomePage /> */}
     </div>
   );
 }
